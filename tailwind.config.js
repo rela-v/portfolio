e787eed29ac0ab0362f9d6fe9e418561f3e1d63c
rela <rela.mngrbot@gmail.com>
2023-06-26 21:52:07 -0700

    adding files and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -6,13 +7,67 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      // iPhone SE
+      '2xs': '300px',
+      sm: '320px',
+      md: '744px',
+      lg: '1280px'
     },
+    colors: {
+      transparent: 'transparent',
+      citrus: {
+        200: '#fdcc8d',
+        600: '#fcb355',
+        800: '#fb991c',
+      },
+      black: '#1a091a',
+      white: '#fff',
+      slate: {
+        200: '#D0D0D3',
+        600: '#B9B9BC',
+        800: '#A1A1A6',
+      },
+      stone: '#504d5d',
+      cloud: '#1a091a'
+    },
+    fontSize: {
+      sm: ['1rem', {
+        lineHeight: '1.25rem',
+        letterSpacing: '0.008em',
+        fontWeight: '400',
+      }],
+      base: ['1rem', {
+        lineHeight: '1.25rem',
+        letterSpacing: '0.002em',
+        fontWeight: '400',
+      }],
+      xl: ['1.5rem', {
+        lineHeight: '1.30rem',
+        letterSpacing: '0em',
+        fontWeight: '500',
+      }],
+      '2xl': ['2.0rem', {
+        lineHeight: '1.36rem',
+        letterSpacing: '0em',
+        fontWeight: '600',
+      }],
+      '3xl': ['2.5rem', {
+        lineHeight: '1.39rem',
+        letterSpacing: '-0.005em',
+        fontWeight: '600',
+      }],
+      '4xl': ['3.0rem', {
+        lineHeight: '1.42rem',
+        letterSpacing: '-0.01em',
+        fontWeight: '700',
+      }],
+      '5xl': ['4.0rem', {
+        lineHeight: '1.45rem',
+        letterSpacing: '-0.02em',
+        fontWeight: '700',
+      }]
+    }
   },
   plugins: [],
 }
